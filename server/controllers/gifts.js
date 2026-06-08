1ac635d933f24663afe8f6d24a880f489590d5e7
Hevander27 <Hevander27gmail.com>
2026-06-08 02:29:57 -0400

    Refactor SQL query in getGifts function to select specific fields

--- server/controllers/gifts.js
+++ server/controllers/gifts.js
@@ -2,7 +2,7 @@ import { pool } from '../config/database.js'
 
 const getGifts = async (req, res) => {
     try {
-        const results = await pool.query('SELECT * FROM gifts ORDER BY id ASC')
+        const results = await pool.query('SELECT id, name, pricePoint AS "pricePoint", audience, image, description, submittedBy AS "submittedBy", submittedOn AS "submittedOn" FROM gifts ORDER BY id ASC')
         res.status(200).json(results.rows)
     } catch (error) {
         res.status(409).json( { error: error.message } )
